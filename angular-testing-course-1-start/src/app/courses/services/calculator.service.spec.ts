2052e1a7672d78ebf683a91014e7210218affb77
William King <wpk12345@gmail.com>
2019-10-03 11:26:39 -0400

    initial commit

---
+++ angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,28 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+
+describe('CalculatorService', () => {
+
+    it('should add two numbers', () => {
+
+        //creating mock here of LoggerService log function
+        const logger = jasmine.createSpyObj('LoggerService', ["log"]);
+
+        const calculator = new CalculatorService(logger);
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4);
+
+        expect(logger.log).toHaveBeenCalledTimes(1);
+
+    });
+
+    it('should subtract two numbers', () => {
+
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.subtract(2, 2);
+       
+        expect(result).toBe(0, "unexpected subtaction result");
+    })
+});
